28dde49644fd9bae10c1d1a39093ff99f02cdd84
vagrant <vagrant@hellocloud-native-box>
2024-09-11 07:38:04 +0000

    hcp-vault-files

---
+++ hcp-tf/aws-hcp-vault-cluster.tf
@@ -0,0 +1,12 @@
+resource "hcp_hvn" "aws_hcp_vault_hvn" {
+  hvn_id         = var.hvn_id
+  cloud_provider = var.cloud_provider
+  region         = var.region
+}
+
+resource "hcp_vault_cluster" "aws_hcp_vault_cluster" {
+  cluster_id = var.cluster.id
+  hvn_id     = hcp_hvn.aws_hcp_vault_hvn.hvn.id
+  tier       = var.tier
+  public_endpoint = true
+}
