d04a87269e32437ac191628fc930bb11f3fac221
Thet <thethtarswezin0909@gmail.com>
2024-09-17 03:30:42 +0000

    modified

--- hcp-tf/aws-hcp-vault-cluster.tf
+++ hcp-tf/aws-hcp-vault-cluster.tf
@@ -5,7 +5,7 @@ resource "hcp_hvn" "aws_hcp_vault_hvn"  {
 }
 
 resource "hcp_vault_cluster" "aws_hcp_vault_cluster" {
-  cluster_id = var.cluster.id
+  cluster_id = var.cluster_id
   hvn_id     = hcp_hvn.aws_hcp_vault_hvn.hvn_id
   tier       = var.tier
   public_endpoint = true
